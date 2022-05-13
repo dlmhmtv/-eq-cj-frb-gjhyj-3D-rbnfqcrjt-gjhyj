89a05adc41fd7440292660af210917be70ee8901
user <user@101-4>
2022-05-13 14:25:32 +0400

    Добавьте файлы проекта.

---
+++ [eq cj,frb gjhyj 3D rbnfqcrjt gjhyj/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _eq_cj_frb_gjhyj_3D_rbnfqcrjt_gjhyj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b;
+            
+            Console.WriteLine("Введите число 1");
+
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+            Console.WriteLine("Введите число 2");
+
+            str = Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+            int c = a + b;
+
+            Console.WriteLine("Сумма чисел " + c);
+        }
+    }
+}
